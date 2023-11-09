41c0db05161391f99a9a8ba181d195b2e32cdf16
awgraves <andrewgraves92@gmail.com>
2023-11-09 09:58:25 -0500

    consolidate files and skip token print if err detected

--- runtime/run.go
+++ runtime/run.go
@@ -1,8 +1,47 @@
 package runtime
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
+
+func RunFile(filePath string) {
+	bytes, err := os.ReadFile(filePath)
+	if err != nil {
+		printError(fmt.Sprintf("Invalid file path: %s\n", filePath))
+		os.Exit(1)
+	}
+
+	run(string(bytes))
+}
+
+func RunPrompt() {
+	fmt.Print(GREEN)
+	fmt.Println("Lox Shell v0.0")
+	fmt.Print(BLUE)
+	fmt.Println("Type 'exit' to quit.")
+	fmt.Print(RESET_COLOR)
+	fmt.Println()
+
+	promptLoop()
+}
+
+func promptLoop() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("> ")
+		scanner.Scan()
+		line := scanner.Text()
+		if line == "exit" {
+			break
+		}
+		if line == "" {
+			continue
+		}
+		run(line)
+	}
+}
 
 func run(input string) {
 	errReporter := newBasicErrorReporter()
@@ -10,8 +49,9 @@ func run(input string) {
 	scanner.ScanTokens()
 
 	if errReporter.HasError() {
-		printError("Errors found - Would not attempt to execute this code.")
+		printError("Errors found - runtime would not attempt to execute this code.")
 		errReporter.Report()
+		return
 	}
 
 	for _, t := range scanner.Tokens {
